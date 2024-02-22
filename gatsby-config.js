4ae6b7e4c0720d8871bc0053a8bba611cb1e519c
davebanguilan <desbanguilan@gmail.com>
2024-02-22 22:13:40 +0800

    Added back twitterUsername

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     description: 'Dave Banguilan - Hybrid Mobile Application Developer Expert',
     siteUrl: 'https://davebanguilan.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
+    twitterUsername: '@dvbngln'
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
